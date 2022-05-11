909df2699812d879529b7a0a80439c9bdd54ebd0
Lelelass <82147263+Lelelass@users.noreply.github.com>
2022-05-11 11:51:55 +0200

    Developped further function Complete_set_split_filenames() to get all datasets splits by filename

--- Labb/dataset_split.py
+++ Labb/dataset_split.py
@@ -1,13 +1,19 @@
 import pandas as pd
 import os
 
-def Complete_split(df):
-    animals = ('dog', 'cat')
-    for animal in animals:
-        select_sample_by_type(df, animal, 800)
+def Complete_set_split_filenames(df, small_split:tuple):
+    """Runs all the datasplits given user defined parameters"""
+    small_train_sets_by_animal = []
+    for animal in range(2):
+        for split in small_split:
+            small_train_sets_by_animal.append(select_sample_by_type(df, animal, split))
+    
+    small_train_set = pd.concat([small_train_sets_by_animal[0], small_train_sets_by_animal[3]], axis = 0)
+    small_val_set = pd.concat([small_train_sets_by_animal[1], small_train_sets_by_animal[4]], axis = 0)
+    small_test_set = pd.concat([small_train_sets_by_animal[2], small_train_sets_by_animal[5]], axis = 0)
+    return small_train_set, small_val_set, small_test_set 
 
 def get_train_from_path(path:str):
-    #original_train_dir = os.path.abspath("./original_data/train/train")
     train_pictures = os.listdir(path)
     return train_pictures
 
@@ -22,10 +28,11 @@ def gen_filename_dataframe(folder_files_list : list):
 
 
 
-def select_sample_by_type(dataframe, animal:str, sample_size:int, random_state=42):
+def select_sample_by_type(dataframe, animal:bool, sample_size:int, random_state=42):
     """Returns a datframe filtered by animal label and sampled to a specified quantity,
-        the samples are also cleared from the original dataframe to avoid data leakage"""
-    df = dataframe.query(f'label =="{animal}"')
+        the samples are also cleared from the original dataframe to avoid data leakage
+        animal:  0 returns dog, 1 cat"""
+    df = dataframe.query(f'_cat =={animal}')
     df = df.sample(n = sample_size,random_state = random_state)
     dataframe.drop(df.index, axis =0, inplace = True)
     return df
