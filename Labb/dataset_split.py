8e95ad762c62ec61236182419544b44e2bccb0de
Lelelass <82147263+Lelelass@users.noreply.github.com>
2022-05-11 16:53:10 +0200

    Minor changes on functions names and descriptions

--- Labb/dataset_split.py
+++ Labb/dataset_split.py
@@ -13,19 +13,18 @@ def Complete_set_split_filenames(df, small_split:tuple):
     small_test_set = pd.concat([small_train_sets_by_animal[2], small_train_sets_by_animal[5]], axis = 0)
     return small_train_set, small_val_set, small_test_set 
 
-def get_train_from_path(path:str):
-    train_pictures = os.listdir(path)
-    return train_pictures
+def get_filenames_from_path(path:str):
+    pictures_filenames = os.listdir(path)
+    return pictures_filenames
 
 def gen_filename_dataframe(folder_files_list : list):
-    """Takes a list of files in a folder and returns a dataframe with filenames,
+    """Reads a list of files in a folder and returns a dataframe with filenames,
      and corresponding animal label; one_hot encoded"""
     df = pd.DataFrame(folder_files_list, columns = ['filename'])
     #Add error hantering ?
     df['label'] = df.filename.str[:3] #Obs : only functions as 'cat' and 'dog' are both 3 char strings.
     df = pd.get_dummies(df, columns = ['label'], prefix=[''])
     return df
-
 
 
 def select_sample_by_type(dataframe, animal:bool, sample_size:int, random_state=42):
