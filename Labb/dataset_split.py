2cf3eb3e86dfc65a916176644277da1fadb3800b
Lelelass <82147263+Lelelass@users.noreply.github.com>
2022-05-12 17:05:05 +0200

    corrected a bug in select_sample_by_type induced by the previous change of one_hot encoding format in gen_filename_dataframe

--- Labb/dataset_split.py
+++ Labb/dataset_split.py
@@ -31,7 +31,7 @@ def select_sample_by_type(dataframe, animal:bool, sample_size:int, random_state=
     """Returns a datframe filtered by animal label and sampled to a specified quantity,
         the samples are also cleared from the original dataframe to avoid data leakage
         animal:  0 returns dog, 1 cat"""
-    df = dataframe.query(f'_cat =={animal}')
+    df = dataframe.query(f'label =={animal}')
     df = df.sample(n = sample_size,random_state = random_state)
     dataframe.drop(df.index, axis =0, inplace = True)
     return df
