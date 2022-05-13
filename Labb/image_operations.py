d5821b6875597afed5f5560b378e9a2a81356e09
Lelelass <82147263+Lelelass@users.noreply.github.com>
2022-05-13 08:54:28 +0200

    add read and resize image functionality: read_and_resize_images  for extracting image dataset

--- Labb/image_operations.py
+++ Labb/image_operations.py
@@ -1,5 +1,7 @@
 import matplotlib.pyplot as plt 
-
+import seaborn as sns
+import cv2
+import numpy as np
 
 def display_images(filenames_dataframe, directory:str, nrows=2, ncols=5, figsize=(12, 4)):
 
@@ -19,3 +21,30 @@ def display_images(filenames_dataframe, directory:str, nrows=2, ncols=5, figsize
 
 
     fig.subplots_adjust(wspace=0.3, hspace=.1, bottom=0)
+
+def extract_dimesion_distribution(df, directory:str):
+    """Reads pictures from filepath column of dataframe,
+       extracts width and height pixel values and scatter plots.
+       Also returns a list of picture sizes tuples, and of large pictures"""
+    all_dims =[]
+    big_img = []
+    for i, path in df['filename'].items():
+        img = plt.imread(f"{directory}/{path}")
+        all_dims.append((img.shape[0], img.shape[1]))
+        if img.shape[0] > 700 or img.shape[1] > 700:
+            big_img.append(df['filename'][i])
+    
+    #sns.jointplot(data=train_pictures_one_hot_with_dim, x='width', y='height')
+    #plt.scatter(*zip(*all_dims))
+    #plt.show()
+    print(f"oversized pictures : {big_img}")
+    return all_dims, big_img
+
+def read_and_resize_images(df, directory:str, target_size:tuple):
+    images = []
+    for _, path in df['filename'].items():
+        img = plt.imread(f"{directory}/{path}")
+        img = cv2.resize(img, target_size)
+        images.append(img)
+    resized_images_array = np.array(images)
+    return resized_images_array
